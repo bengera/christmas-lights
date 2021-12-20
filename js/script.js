4b702f4118d80b2c884bf714bf849bfc4c0ff55c
unknown <bengera89@gmail.com>
2021-12-20 18:52:14 +0700

    light-pulse

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
 const btnOn = document.querySelector('.on')
 const btnOff = document.querySelector('.off')
 
+// const lights = document.getElementsByClassName('.red.yellow')
+const redLight = document.querySelector('.red')
 
-// TURN ON
+// TURN OFF
 document.querySelectorAll('.circle').forEach(circle => {
     btnOff.addEventListener('click', () => {
         circle.style.backgroundColor = "transparent";
+
+
+        redLight.style.animation = 'none';
         console.log('switch-off');
-        
+
     })
-    
+
 })
 
-// TURN OFF
+// const off = function () {
+//     for (var i = 0; i < length; i++) {
+//         lights[i].style.animation = "none";
+
+//       }
+// }
+
+// TURN ON
 document.querySelectorAll('.circle').forEach(circle => {
     btnOn.addEventListener('click', () => {
         circle.removeAttribute('style');
         console.log('switch-on');
-        
+
     })
-    
+
 })
 
 
 
 
+
+
+
+
+//  for(var i = 0; i < circleLights.length; i++) {
+//     circleLights[i].style.animation= 'none';
+//   }
+//   console.log(circleLights);
